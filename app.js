8928fb6c158bf8bb8385b821cb2465d5feac7f44
Andrzej Regulski <a.regulski@gmail.com>
2023-02-17 15:13:02 +0100

    add coloring script

--- app.js
+++ app.js
@@ -2,5 +2,16 @@
 const button = document.querySelector('#button-color');
 
 button.addEventListener('click', function() {
-    console.log("blah!")
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const newColor = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.backgroundColor = newColor;
+    const div = document.createElement("div");
+    const par = document.createElement("p");
+    const text = document.createTextNode(newColor);
+    par.appendChild(text);
+    div.appendChild(par);
+    div.style.backgroundColor = newColor;
+    document.body.appendChild(div);
 })
