9cdb5b5c67c49dc09be6484d1a9d060135056efc
Andrzej Regulski <a.regulski@gmail.com>
2023-02-18 11:31:05 +0100

    coloring narrowed - blueish

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const button = document.querySelector('#button-color');
 
 button.addEventListener('click', function() {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
+    const r = Math.floor(Math.random() * 30 + 160);
+    const g = Math.floor(Math.random() * 30 + 200);
+    const b = Math.floor(Math.random() * 30 + 210);
     const newColor = `rgb(${r}, ${g}, ${b})`;
     document.body.style.backgroundColor = newColor;
     const div = document.createElement("div");
